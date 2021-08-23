8640a1181d5d52eab7a792407486524f8f3182c8
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-08-23 11:14:36 +0430

    added mapbox and its access token

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
+// LIBRARIES:
+import React, { useState } from "react";
+import ReactMapGL from "react-map-gl";
+
+// FILES:
 import "./App.css";
 
 function App() {
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8,
+  });
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_KEY}
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+    />
   );
 }
 
