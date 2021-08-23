dc28376c184c4c39a6a694b99e77fd3bc5c78b05
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-08-23 14:17:00 +0430

    added the first viewport and its marker on berlin as a default state

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
 // LIBRARIES:
-import React, { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import React, { useState, useMemo } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
 
 // FILES:
+import "mapbox-gl/dist/mapbox-gl.css";
 import "./App.css";
 
 function App() {
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8,
+    width: "100vw",
+    height: "100vh",
+    latitude: 52,
+    longitude: 13,
+    zoom: 4,
   });
+
+  const markers = useMemo(() => {
+    return (
+      <Marker latitude={52.520008} longitude={13.404954}>
+        <div>You are here</div>
+      </Marker>
+    );
+  }, []);
 
   return (
     <ReactMapGL
       {...viewport}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_KEY}
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
-    />
+    >
+      {markers}
+    </ReactMapGL>
   );
 }
 
