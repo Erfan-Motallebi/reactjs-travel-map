00a48cdd41d570e0ff837475ef84d837ab034305
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-09-01 12:31:27 +0430

    used React-hook-form instead of basic form

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,38 +1,32 @@
 import { TextField, Button } from "@material-ui/core";
 import React, { useState } from "react";
 import { Room as RoomIcon, CloseSharp } from "@material-ui/icons";
+import { useForm } from "react-hook-form";
 
 function Login({ loginCb, setOpen }) {
-  const [userLogin, setUserLogin] = useState({
-    username: "",
-    password: "",
-  });
-  const registerHandler = (e) => {
-    e.preventDefault();
-    loginCb(userLogin);
+  const { register, handleSubmit, watch, formState } = useForm();
+
+  const registerHandler = (data) => {
+    loginCb(data);
   };
 
   return (
     <div className="modal-container-login">
       <RoomIcon className="roomIcon" />
       <CloseSharp className="btn-close" onClick={setOpen.bind(this, false)} />
-      <form className="form-login" onSubmit={registerHandler}>
+      <form className="form-login" onSubmit={handleSubmit(registerHandler)}>
         <TextField
           label="Username"
           id="username"
           variant="outlined"
-          onChange={(e) =>
-            setUserLogin({ ...userLogin, username: e.target.value })
-          }
+          {...register("username")}
         />
         <TextField
           label="Password"
           id="password"
           type="password"
           variant="outlined"
-          onChange={(e) =>
-            setUserLogin({ ...userLogin, password: e.target.value })
-          }
+          {...register("password")}
         />
         <Button
           style={{
