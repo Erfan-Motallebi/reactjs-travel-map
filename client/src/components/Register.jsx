ecfff8cc4c7fa6a917494eaedda7a5d85351435d
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-09-02 06:36:09 +0430

    register fields error handling with react-hook-form

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -2,9 +2,14 @@ import { TextField, Button } from "@material-ui/core";
 import React from "react";
 import { Room as RoomIcon, CloseSharp } from "@material-ui/icons";
 import { useForm } from "react-hook-form";
+import Flip from "react-reveal/Flip";
 
 function Register({ registerHandlerCb, setOpen }) {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const registerHandler = (data) => {
     registerHandlerCb(data);
   };
@@ -20,11 +25,34 @@ function Register({ registerHandlerCb, setOpen }) {
           variant="outlined"
           {...register("username", {
             required: true,
-            minLength: 3,
+            minLength: 5,
             maxLength: 30,
             pattern: /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/gim,
           })}
         />
+        {errors.username?.type === "required" && (
+          <Flip>
+            <h5 style={{ color: "red" }}>Username is required</h5>
+            <Flip></Flip>
+          </Flip>
+        )}
+        {errors.username?.type === "minLength" && (
+          <Flip>
+            <h5 style={{ color: "red" }}>Username is more than 5 letters.</h5>
+          </Flip>
+        )}
+        {errors.username?.type === "Pattern" && (
+          <Flip>
+            <h5 style={{ color: "red" }}>Right pattern is required.</h5>
+          </Flip>
+        )}
+        {errors.username?.type === "maxLength" && (
+          <Flip>
+            <h5 style={{ color: "red" }}>
+              Maximum length is less than 30 letters.
+            </h5>
+          </Flip>
+        )}
         <TextField
           label="Email"
           id="email"
@@ -35,6 +63,22 @@ function Register({ registerHandlerCb, setOpen }) {
             pattern: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g,
           })}
         />
+        {errors.email?.type === "required" && (
+          <Flip>
+            <h5>Email is required</h5>
+          </Flip>
+        )}
+        {errors.email?.type === "maxLength" && (
+          <Flip>
+            <h5>Maximum length is less than 50 letters.</h5>
+          </Flip>
+        )}
+        {errors.email?.type === "pattern" && (
+          <Flip>
+            <h5>Right pattern is required. please try again </h5>
+          </Flip>
+        )}
+
         <TextField
           label="Password"
           id="password"
@@ -42,6 +86,10 @@ function Register({ registerHandlerCb, setOpen }) {
           variant="outlined"
           {...register("password", { required: true, minLength: 6 })}
         />
+        {errors.password?.type === "required" && <h5>Password is required</h5>}
+        {errors.password?.type === "minLength" && (
+          <h5>Minimum length is over 6 letters.</h5>
+        )}
         <Button
           style={{
             background: "lightblue",
